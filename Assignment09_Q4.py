5ca352a9d1def2489781026e2f109aea7cc3cb50
Prathamesh Zarekar <prathmeshszarekar@gmail.com>
2026-06-30 18:39:40 +0530

    Cube of a number

---
+++ Assignment09_Q4.py
@@ -0,0 +1,18 @@
+# Write a program which accepts one number and prints cube of that number 
+
+# Scripting
+number = int(input("Enter Number: "))
+cube = number*number*number
+print("Cube of ",number," is : ",cube)
+
+#Procedural
+'''def cube(value):
+    return value*value*value
+
+def main():
+    number1 = int(input("Enter number: "))
+    Ret = cube(number1)
+
+    print("pcube of ",number1," is: ",Ret)
+
+main()'''
