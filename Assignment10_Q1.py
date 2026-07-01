f1c8109c6e31f8b2c3d9dd12c26b7a475d391551
Prathamesh Zarekar <prathmeshszarekar@gmail.com>
2026-07-01 22:14:29 +0530

    multiplication Table

---
+++ Assignment10_Q1.py
@@ -0,0 +1,5 @@
+# Write a program which accepts one number and prints multiplication table of that number 
+
+number = int(input("Enter the number: "))
+for no in range(1,11):
+    print(number, "X" ,no, "=",number*no)
