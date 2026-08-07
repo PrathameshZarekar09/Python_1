6e34d5646e988a62c146a00f4ab637504a13d95f
Prathamesh Zarekar <prathmeshszarekar@gmail.com>
2026-08-07 22:15:09 +0530

    Program to display total number of student, count of student failed and passed

---
+++ Assignment38_Q2.py
@@ -0,0 +1,38 @@
+import pandas as pd 
+
+Border = "-"*35
+
+Datapath = "student_performance_ml.csv"
+
+df = pd.read_csv(Datapath)
+
+###########################################
+# Total number of students
+###########################################
+print(Border)
+print("Total number of students: ",len(df))
+print(Border)
+
+
+
+###########################################
+# Student Passed (Final Result = 1)
+###########################################
+print(Border)
+count=0
+for result in df["FinalResult"]:
+    if result == 1:
+        count = count+1
+print("Student Passed: ",count)
+
+
+
+###########################################
+# Student failed (Final Result = 0)
+###########################################
+print(Border)
+count=0
+for result in df["FinalResult"]:
+    if result == 0:
+        count = count+1
+print("Student Failed: ",count)
